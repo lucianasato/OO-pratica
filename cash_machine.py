df09e3d9447662d5acee206ad7405ee5f5706952
Luciana Sato <sato.lu@gmail.com>
2019-08-18 16:49:13 -0300

    Modele as regras de negócio

---
+++ cash_machine.py
@@ -0,0 +1,13 @@
+class BankAccount:
+    def __init__(self, account_number, name, password, value, admin):
+        self.account_number = account_number
+        self.name = name
+        self.password = password
+        self.value = value
+        self.admin = admin
+
+accounts_list = [
+    BankAccount('0001-02', 'Fulano 1', '123456', 100, False),
+    BankAccount('0002-02', 'Fulano 2', '123456', 50, False),
+    BankAccount('1111-11', 'Fulano 3', '123456', 1000, True)
+]
