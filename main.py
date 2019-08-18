aa28be4c2b1e76c17f42f08c78565c1a1ce59534
Luciana Sato <sato.lu@gmail.com>
2019-08-18 14:42:18 -0300

    Added initial file

---
+++ main.py
@@ -0,0 +1,11 @@
+from utils import clear, header
+
+def main():
+    clear()
+    header()
+
+if __name__ == '__main__':
+    while True:
+        main()
+
+        input("Pressione <ENTER> para continuar...")
---
+++ utils.py
@@ -0,0 +1,10 @@
+import os
+
+def clear():
+    clear = 'cls' if os.name == 'nt' else 'clear'
+    os.system(clear)
+
+def header():
+    print("**************************************")
+    print("********* Caixa Eletrônico ***********")
+    print("**************************************")
